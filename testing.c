6007ededb75ab540bca867de7d7092d80b9fedbc
Pedro de Medeiros <pedro.medeiros@gmail.com>
2025-04-19 11:01:17 -0300

    more useful example

--- testing.c
+++ testing.c
@@ -1,14 +1,37 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include "printf.h"
 
 
-uint8_t v[] = {
-	0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9,
-};
-
-
-void main(void)
+int function_to_move(int a, int b)
 {
-	printf("first element: %i\r\n", v[0]);
-	printf("second element: %i\r\n", v[1]);
+	return a + b;
+}
+int function_end_marker()
+{
+	return 0;
+}
+
+// function signature
+typedef int (*callback_t)(int, int);
+// first available bank 3 position
+static uint8_t* last_mem = 0xc000;
+callback_t move_to_high_mem(const callback_t function, uint16_t len, void* dest)
+{
+	memcpy((uint8_t*) dest, (const uint8_t*) function, len);
+
+	// save size and add it to last_mem
+	callback_t old_last_mem = (callback_t)last_mem;
+	last_mem += len;
+
+	return old_last_mem;
+}
+
+int main()
+{
+	int size = (void*)function_end_marker - (void*)function_to_move;
+	callback_t new_function = move_to_high_mem(function_to_move, size, last_mem);
+	// call function copy from bank 3
+	return new_function(2, 3);
 }
